ca8e56723e8d9ccfc69e39ce9838edbc02eabc13
Negin Shafaei <36847007+neginshafaei@users.noreply.github.com>
2025-12-19 15:48:16 +0330

    Time: 4 ms (90.49%), Space: 59.2 MB (75.51%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
+    let shallowS = []
+    let char = s.split('')
+
+    let left = 0
+    let right = char.length - 1
+
+    while (left < right) {
+
+        while (left < right && !vowels.includes(char[left])) {
+            left ++
+        }
+
+        while (left < right && !vowels.includes(char[right])) {
+            right --
+        }
+
+        if (left < right) {
+            [char[left], char[right]] = [char[right], char[left]]
+            right --
+            left ++
+        }
+    }
+    return char.join('')
+};
